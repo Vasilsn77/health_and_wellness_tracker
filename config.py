0113cf39baf36670312de639b301066f257ff47e
Vasil S N <140039563+Vasilsn77@users.noreply.github.com>
2024-11-17 23:55:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',  # Your MySQL username
+    'password': 'vasil',  # Your MySQL password
+    'database': 'HealthAndWellnessTracker'  # Your database name
+}
